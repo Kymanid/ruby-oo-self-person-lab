5aac43a7642c96c3a2678d7cb7a9d76e5bd3c2c9
Kymani Davis <Kymanidavis@gmail.com>
2020-12-08 13:25:54 -0500

    commit tree 6945057e7441acfe088c3966731fc9ac3b456cdb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,48 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialized(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+    end
+
+    def bank_account
+        @bank_account
+    end
+
+    def happiness
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 10
+        @happiness
+    end
+
+    def hygiene
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+        @hygiene
+    end
+
+    def take_bath
+    end
+
+    def work_out
+    end
+
+    def call_friend
+    end
+
+    def start_conversation
+    end
+    
+
+
+
+
+
+
+end
+
+Person.new
